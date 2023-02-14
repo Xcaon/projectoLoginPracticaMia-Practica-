47a3d2df4d06e1c7166af083988d768b1c94ef1d
Xcaon <fernandosteam@hotmail.es>
2023-02-14 20:47:37 +0100

    Diseño y botones funcionando

--- Vista/login.php
+++ Vista/login.php
@@ -15,14 +15,14 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" type="text/css" href="estilos.css" />
+    <link rel="stylesheet" type="text/css" href="css/estilos.css" />
     <title>Document</title>
 </head>
 <body>
 
 <div class="padre">
     <div class="contenedorLogin">
-        <img class="logo" src="http://127.0.0.1/projectoNotasMio/projectoLoginPracticaMia/Vista/imagenes/imagen.png" width="100" height="100">
+        <img class="logo" src="imagenes/imagen.png" width="100" height="100">
         <h1>Iniciar Sesión</h1>
         <form action="login.php" method="POST">
             <label class="labelLogin" aria-placeholder="Introduce tu nombre de usuario.." for="usuario">Usuario</label>
